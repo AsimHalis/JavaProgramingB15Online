e5bd56ddc12b9156b7a19ba532185bdd708bcf86
Asim Halis <asimhcan@gmail.com>
2019-12-23 11:55:04 -0500

    printout new second one

--- src/day32/CharAction2.java
+++ src/day32/CharAction2.java
@@ -33,6 +33,7 @@ public class CharAction2 {
                 }
             else{
                 System.out.println("Charakters are Same");
+                System.out.println("deneme2");
             }
             }
         }
