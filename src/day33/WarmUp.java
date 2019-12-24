a69c6600f26338e4d9e9ce9ae97a6efda649bcd2
Asim Halis <asimhcan@gmail.com>
2019-12-23 19:43:13 -0500

    Day-33 Spelling Name

--- src/day33/WarmUp.java
+++ src/day33/WarmUp.java
@@ -2,6 +2,34 @@ package day33;
 
 public class WarmUp {
     public static void main(String[] args) {
+         String spelledName=getSpelledName("Asim");
+        System.out.println("spelledName = "+ spelledName);
+       System.out.println(getSpelledName("Halis"));
+        System.out.println(getSpelledName2("Furkan"));
+    }
+
+    public static String getSpelledName(String name){
+
+     String result="";
+        for (int x = 0; x <name.length() ; x++) {
+            result=result+name.charAt(x);
+            if(x!=name.length()-1){
+                result = result+ "-" ;
+            }
+        }
+        return result;
 
     }
+    public static String getSpelledName2(String name) {
+
+        String result = "";
+        for (int i = 0; i < name.length() - 1; i++) {
+            result += name.charAt(i) + "-";
+        }
+        return result+name.charAt(name.length()-1);
+    }
+
+
+
+
 }
