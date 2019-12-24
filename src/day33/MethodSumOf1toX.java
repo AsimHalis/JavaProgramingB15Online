b95994a917943cf52b9eb0203620f2f3bd1a5b45
Asim Halis <asimhcan@gmail.com>
2019-12-23 21:33:47 -0500

    Day-33 SumOf1ToX

--- src/day33/MethodSumOf1toX.java
+++ src/day33/MethodSumOf1toX.java
@@ -3,7 +3,7 @@ package day33;
 public class MethodSumOf1toX {
     public static void main(String[] args) {
         System.out.println(sum1ToX(10));
-
+        System.out.println(sum1ToX(15));
     }
     public static int sum1ToX(int X){
         int sum=0;
