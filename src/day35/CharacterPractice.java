3d5870e4c3bd42da957bf32a725bfe2845bc02be
Asim Halis <asimhcan@gmail.com>
2020-01-21 19:54:14 -0500

    day-35 Char Practice

---
+++ src/day35/CharacterPractice.java
@@ -0,0 +1,11 @@
+package day35;
+
+public class CharacterPractice {
+    public static void main(String[] args) {
+        System.out.println(Character.isDigit('A'));//false
+        System.out.println(Character.isDigit(' '));//false
+        System.out.println(Character.isDigit('6'));//true
+
+
+    }
+}
