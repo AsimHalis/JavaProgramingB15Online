68883324810012e21c148c51739f2a9f69f1c244
Halis-Can <haliscan@hotmail.com>
2020-04-19 11:52:51 -0400

    day-53 Interface

--- src/OfficeHours/MethodPracticePart2.java
+++
@@ -1,7 +0,0 @@
-package OfficeHours;
-
-public class MethodPracticePart2 {
-    public static void main(String[] args) {
-
-    }
-}
--- src/day70/gitHubTrye.java
+++
@@ -1,8 +0,0 @@
-package day70;
-
-public class gitHubTrye {
-
-    public static void main(String[] args) {
-        System.out.println("hello github");
-    }
-}
