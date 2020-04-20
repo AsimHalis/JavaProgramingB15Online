c6643b0ac7e50fefb041297486d0088dff146265
Halis-Can <haliscan@hotmail.com>
2020-04-20 17:00:02 -0400

    day-53 Interface

---
+++ src/day53_InterfaceRecap/A.java
@@ -0,0 +1,37 @@
+package day53_InterfaceRecap;
+
+
+interface B{
+void method1();
+}
+
+public interface A extends B {// if both type is same use the extend kewword!!! B is called supertype, A is subtype
+                              // A is inhered all from B interface
+    void method2();
+    void method3();
+   //void method1();        //==>you cant see but you still have method(1) abstract method
+
+
+}
+
+class C implements A {// if both type is different use the implemnets keyword!!!
+                    // now we have 3 absract method  overridee
+    @Override
+    public void method1() {
+
+    }
+
+    @Override
+    public void method2() {
+
+    }
+
+    @Override
+    public void method3() {
+
+    }
+}
+
+class D extends C implements A,B{
+
+}
