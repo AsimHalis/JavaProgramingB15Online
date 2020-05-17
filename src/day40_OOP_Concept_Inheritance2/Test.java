f6498a742fc9ccfdc7d57e40948e7714794eb738
Halis-Can <haliscan@hotmail.com>
2020-05-17 17:43:41 -0400

    day40 OOP Concepts3

--- src/day40_OOP_Concept_Inheritance2/Test.java
+++ src/day40_OOP_Concept_Inheritance2/Test.java
@@ -3,6 +3,7 @@ package day40_OOP_Concept_Inheritance2;
 public class Test {
     public static void main(String[] args) {
         Dog dog=new Dog("Dodo",20,12,4);
-            dog.animalSpeed(20);
+            dog.animalSpeed(20);// Dog runing 20
+                                //Animal runing 30
     }
 }
